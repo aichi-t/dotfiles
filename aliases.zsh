9ce5179868b3683b7d94b4e876154d6f11409e2d
Aichi Tsuchihira <aichitsuchihira@gmail.com>
2022-04-06 16:55:08 +1000

    Update alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,6 @@
 alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
-alias htdc-rails="foreman start -f Procfile.dev -e .env.dev"
+alias r="foreman start -f Procfile.dev -e .env.dev"
 alias c='code .'
 unalias l
 alias l='lazygit'
-alias y='yarn start'
+alias y='yarn && yarn start'
