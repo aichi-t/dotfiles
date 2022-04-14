a4f80f96d4ea100650f40fecbf5e8ca00e0fe96b
Aichi Tsuchihira <aichitsuchihira@gmail.com>
2022-04-15 00:50:25 +1000

    Remove unalias

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,5 @@
 alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 alias r="foreman start -f Procfile.dev -e .env.dev"
 alias c='code .'
-unalias l
 alias l='lazygit'
 alias y='yarn && yarn start'
